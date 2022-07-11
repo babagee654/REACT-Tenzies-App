c3476f240bd1cdd5592f8a1d6814a769c09d1e45
Babagee654 <jerome_acosta@hotmail.com>
2022-07-11 14:29:48 -0400

    Added reroll function, and removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
+import React from "react";
 import Die from "./components/Die";
 
-
-function allNewDice() {
-    let randomNums = []
-    for (let i = 0; i < 10; i++) {
-        let rand = Math.floor((Math.random() * 6) + 1);
-        randomNums.push(rand)
-    }
-    return randomNums
-}
-
-allNewDice()
-
 export default function App() {
+
+    function allNewDice() {
+        let randomNums = []
+        for (let i = 0; i < 10; i++) {
+            let rand = Math.floor((Math.random() * 6) + 1);
+            randomNums.push(rand)
+        }
+        return randomNums
+    }
+
+    const [dice, setDice] = React.useState(allNewDice())
+
+    let diceElements = dice.map(die => {
+        return <Die value={die} />
+    })
+
+    function reroll(event) {
+        setDice(allNewDice())
+    }
+
     return (
         <main>
             <div className="die--container">
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
+                {diceElements}
             </div>
+            <button onClick={reroll}>Reroll!</button>
         </main>
     )
 }
---
+++ src/components/Die.js
@@ -0,0 +1,7 @@
+export default function Die(props) {
+    return (
+        <div className="die">
+            <h3 className="die--num">{props.value}</h3>
+        </div>
+    )
+}
