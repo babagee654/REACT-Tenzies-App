f4659941b98860cfe51eed4db82068e5cf53dbf0
Babagee654 <jerome_acosta@hotmail.com>
2022-07-13 14:28:44 -0400

    Added dice holding, and win condition

--- src/App.js
+++ src/App.js
@@ -1,33 +1,72 @@
 import React from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
+    //States
+    const [win, setWin] = React.useState(false)
+    const [dice, setDice] = React.useState(allNewDice())
 
+    // useEffects
+    React.useEffect(() => {
+        const allHeld = dice.every(dice => dice.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(dice => dice.value === firstValue)
+        if (allHeld && allSameValue) {
+            setWin(true)
+            console.log("You won!")
+        }
+    }, [dice])    // Run this effect if dice state has changed. 
+
+    //Functions 
     function allNewDice() {
         let randomNums = []
         for (let i = 0; i < 10; i++) {
-            let rand = Math.floor((Math.random() * 6) + 1);
-            randomNums.push(rand)
+            let value = Math.floor((Math.random() * 6) + 1);
+            randomNums.push({ value: value, isHeld: false, id: nanoid() })
         }
         return randomNums
     }
 
-    const [dice, setDice] = React.useState(allNewDice())
+    function reroll() {
+        setDice(oldDice => {
+            // if isHeld is true, keep it, else reroll it.
+            return oldDice.map(dice => {
+                let reroll = Math.floor((Math.random() * 6) + 1);
+                return dice.isHeld ? dice : { ...dice, value: reroll }
+            })
+        })
+    }
 
+    function toggleHold(id) {
+        // console.log("Connected")
+        // console.log(id)
+        setDice(oldDice => {
+            return oldDice.map(dice => {
+                return dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice
+            })
+        })
+    }
+
+    // Array of Elements
     let diceElements = dice.map(die => {
-        return <Die value={die} />
+        return <Die
+            value={die.value}
+            isHeld={die.isHeld}
+            key={die.id}
+            id={die.id}
+            toggleHold={toggleHold}
+        />
     })
 
-    function reroll(event) {
-        setDice(allNewDice())
-    }
+
 
     return (
         <main>
             <div className="die--container">
                 {diceElements}
             </div>
-            <button onClick={reroll}>Reroll!</button>
+            <button className="reroll--button" onClick={reroll}>Reroll</button>
         </main>
     )
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,6 +1,6 @@
 export default function Die(props) {
     return (
-        <div className="die">
+        <div className={`die ${props.isHeld ? "held" : ""}`} onClick={() => props.toggleHold(props.id)}>
             <h3 className="die--num">{props.value}</h3>
         </div>
     )
